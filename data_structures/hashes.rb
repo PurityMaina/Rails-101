0c25f419ce1c1e13237773de3a5d266268412814
Purity <pmaina@lipalater.com>
2019-02-25 12:50:43 +0300

    Added hashes

--- data_structures/hashes.rb
+++ data_structures/hashes.rb
@@ -1,5 +1,27 @@
 #hashes are key-value pairs
 
+class Book
+  attr_reader :author, :title
+
+  def initialize(author, title)
+    @author = author
+    @title = title
+  end
+
+  def ==(other)
+    self.class === other and
+      other.author == @author and
+      other.title == @title
+  end
+
+  alias eql? ==
+
+  def hash
+    @author.hash ^ @title.hash # XOR
+  end
+end
+
+#literal hashes
 hash_example = {
 	"key1" => "value1",
 	"key2" => "value2",
@@ -10,3 +32,7 @@ puts "The whole hash is #{hash_example}"
 
 puts "The first key is #{hash_example["key1"]}"
 puts "The first value is #{hash_example["value1"]}, meeh you can only reference by keys!"
+
+#A Hash can also be created through its ::new method:
+grades = Hash.new
+grades["Dorothy Doe"] = 9
